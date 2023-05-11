ad5c3b95d4eaa03b846e2c9bf9a7df74d67766cd
Bella-LYT <33800363+Bella-LYT@users.noreply.github.com>
2023-05-11 11:07:01 +0800

    Add files via upload

--- codex.py
+++ codex.py
@@ -13,19 +13,24 @@ import openai
 下述模型中gpt-3.5-turbo是chatgpt模型的api接口，是GPT-3.5系列中最快速、最便宜、最灵活的模型。
 """
 
-openai.api_key = "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" #openai的api_key
+
 
 #下述请求text-davinci-003的回应
-response = openai.Completion.create(
+def generate_seeds(prompts):
+  openai.api_key = "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" #openai的api_key
+  response = openai.Completion.create(
   model="text-davinci-003",
-#   engine = "code-davinci-003",
-  prompt="\"\"\"\n0、create a function named f to do the following steps 1、Import TensorFlow 2.10.0 2、Generate input data 3、Call the API tf.nn.conv2d(input,filters,strides, padding,data_format='NHWC',dilations=None,name=None)\n\"\"\"",
+  #   engine = "code-davinci-003",
+  prompt = prompts,
+  # prompt="\"\"\"\n 1、Import TensorFlow 2.10.0 2、Generate input data 3、Call the API tf.nn.conv2d(input,filters,strides, padding,data_format='NHWC',dilations=None,name=None)\n\"\"\"",
   temperature=0,
   max_tokens=256,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
-)
+  )
+  result_code = json.loads(str(response))['choices'][0]['text']  #str
+  return result_code
 
 #下述请求chatgpt的回应
 response1 = openai.ChatCompletion.create(
